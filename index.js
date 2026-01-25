5408857f8e6d5494d4fa77c1574123c36261564d
Johnathan Wu <143577305+jwu018@users.noreply.github.com>
2026-01-24 22:20:06 -0600

    Adding web files from IDE

---
+++ index.js
@@ -0,0 +1,28 @@
+// Dark Mode Logic
+const toggleButton = document.getElementById('theme-toggle');
+let darkMode = false;
+let svgimg = document.getElementById('dark-moder');
+
+function darkOn() {
+  document.body.classList.toggle('dark-mode');
+  svgimg.setAttribute('d', "M 11 0 L 11 3 L 13 3 L 13 0 L 11 0 z M 4.2226562 2.8085938 L 2.8085938 4.2226562 L 4.9296875 6.34375 L 6.34375 4.9296875 L 4.2226562 2.8085938 z M 19.777344 2.8085938 L 17.65625 4.9296875 L 19.070312 6.34375 L 21.191406 4.2226562 L 19.777344 2.8085938 z M 12 5 A 7 7 0 0 0 5 12 A 7 7 0 0 0 12 19 A 7 7 0 0 0 19 12 A 7 7 0 0 0 12 5 z M 0 11 L 0 13 L 3 13 L 3 11 L 0 11 z M 21 11 L 21 13 L 24 13 L 24 11 L 21 11 z M 4.9296875 17.65625 L 2.8085938 19.777344 L 4.2226562 21.191406 L 6.34375 19.070312 L 4.9296875 17.65625 z M 19.070312 17.65625 L 17.65625 19.070312 L 19.777344 21.191406 L 21.191406 19.777344 L 19.070312 17.65625 z M 11 21 L 11 24 L 13 24 L 13 21 L 11 21 z")
+}
+
+function darkOff() {
+  document.body.classList.toggle('dark-mode');
+  svgimg.setAttribute('d', "M12 3a9 9 0 1 0 9 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 0 1-4.4 2.26 5.403 5.403 0 0 1-3.14-9.8c-.44-.1-.9-.1-1.36-.1z");
+}
+
+function toggleTheme() {
+  if (darkMode) {
+    darkOff();
+    darkMode = false;
+  } else {
+    darkOn();
+    darkMode = true;
+  }
+}
+
+if (toggleButton) {
+  toggleButton.addEventListener('click', toggleTheme);
+}
